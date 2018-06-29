f148be0ab7e9ffa8ed3bc0f3793480eb1a4678c0
herms97 <35726959+herms97@users.noreply.github.com>
2018-06-30 01:17:00 +0200

    implemented core game functions

--- Ball.java
+++ Ball.java
@@ -1,35 +1,68 @@
-package Breakout;
+package newpackage;
 
 import java.awt.Color;
 import java.awt.Graphics;
 
 public class Ball {
 	static float x;
-	static float xVel = 10f;
+        static float velocity = 1;
+	static float xVel = 2;
 	static float y;
-	static float yVel = 5f;
-	public void Ball(float x, float y){
+	static float yVel = 2;
+        int points = 0;
+
+        
+	Ball(float x, float y){
 		this.x = x;
 		this.y = y;
 	}
 	
 	//Moves the x and y with xVel and yVel
 	public static void move(){
-		x += xVel;
-		y += yVel;
+                velocity += 0.001f;
+                System.out.println(velocity);
+		x += xVel*velocity;
+		y += yVel*velocity;
 		if(x>=1000 || x<=0)
 			xVel = -xVel;
-		if(y>600 || y<0)
+		if(y<0)
 			yVel = -yVel;
 	}
 	
+        
+        
+        public void checkCollision(Player p){
+            if(y >= p.getY() - 20 && y <= p.getY() + p.getHeight()){
+                if(x >= p.getX() && x < p.getX() + p.getWidth()){
+                    yVel = -yVel;
+                    points += 10;
+                    //bounceBack(p);
+                }
+            }
+        }
+        
+        
+        public int getPoints(){
+            
+            return points;
+        }
+        
 	//calculates how the ball bounces off Player
-	public void bounceBack(){
-		//
+	public void bounceBack(Player p){
+            
+            float center = p.getX() + (p.getWidth()*0.5f);
+            xVel = (float) Math.cos(center) * velocity;
+            yVel = (float) Math.sin(center) * velocity;
 	}
 	
+        public float getY(){
+            
+            return y;
+        }
+        
 	public static void draw(Graphics g) {
 		g.setColor(Color.white);
 		g.fillRect((int) x, (int)y, 20, 20);
 	}
+        
 }
--- Breakout.java
+++ Breakout.java
@@ -1,4 +1,4 @@
-package Breakout;
+package newpackage;
 
 import java.applet.*;
 import java.awt.Color;
@@ -18,12 +18,13 @@ public class Breakout extends Applet implements Runnable, KeyListener {
 	Graphics gfx;
 	final int WIDTH = 1000, HEIGHT = 600; 
 	Image img;
+        boolean gameStarted = false;
 	
 	//TODO: Herman fixi. Skriv metoden for player
-	//Player player = new Player();
+	Player player = new Player(450, 300);
 	
 	//TODO: Ball klasse. Heems eller evvs fix
-	Ball ball = new Ball();
+	Ball ball = new Ball(500, 0);
 	
 	//Method initializes application
 	public void init(){
@@ -34,10 +35,27 @@ public class Breakout extends Applet implements Runnable, KeyListener {
 		thread = new Thread(this);
 		thread.start();
 	}
+        
+        public void startGame(){
+            gameStarted = true;
+        }
+        
+        public void gameOver(){
+                gameStarted = false;
+                ball = new Ball(500, 0);
+                player = new Player(450, 300); 
+        }
 	//Updates per cycle
 	public void update(Graphics gfx){
-		Ball.move();;
-		paint(gfx);
+            if(gameStarted){
+		Ball.move();
+                ball.checkCollision(player);                
+                player.move(); 
+                if(ball.getY()>=600)
+                    gameOver();
+            }
+                paint(gfx);
+
 	}
 	
 	@Override
@@ -49,31 +67,39 @@ public class Breakout extends Applet implements Runnable, KeyListener {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+                        
 		}
 	}
 	
 	public void paint(Graphics g){
 		gfx.setColor(Color.BLACK);
 		gfx.fillRect(0, 0, WIDTH, HEIGHT);
-		gfx.setColor(Color.black);
-		gfx.drawString("TEST TEST TEST TEST TEST", 340, 30);
+		gfx.setColor(Color.white);
+                if(!gameStarted)
+                    gfx.drawString("Press enter to begin", 460, 300);
 		Ball.draw(gfx);
-		//draw ball, player etc...
+                gfx.drawString("Points: " + ball.getPoints(), 0, 600 );
 		
+                player.draw(gfx);
 		g.drawImage(img, 0, 0, this);
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_ENTER){
-			//Det som skjer når du trykke ENTER
+			//Det som skjer nï¿½r du trykke ENTER
 		}
+                player.keyPressed(e, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT);
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+		if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                    startGame();
+			//Det som skjer nï¿½r du trykke ENTER
+		}
+                player.keyReleased(e, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT);
 		
 	}
 
